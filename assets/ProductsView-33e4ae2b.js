193478fa0e57fd4c2b6b17580f10757ced84e1c9
masakiLee <everything2427@hotmail.com>
2023-02-24 15:50:39 +0800

    deploy

---
+++ assets/ProductsView-33e4ae2b.js
@@ -0,0 +1,1 @@
+import{_ as s,o as e,c as o,a as t}from"./index-1145b443.js";const c={},a={class:"about"},n=t("h2",null,"This is an Products pages",-1),_=[n];function r(d,i){return e(),o("div",a,_)}const u=s(c,[["render",r]]);export{u as default};

158cc8d33a84c4800d7d38bf748bdeaf64377699
masakiLee <everything2427@hotmail.com>
2023-02-24 16:29:13 +0800

    Two

--- src/components/HomeBanner.vue
+++ src/components/HomeBanner.vue
@@ -3,10 +3,10 @@ export default {
   data() {
     return {
       banner: [
-        { images: "../assets/image/bannerOne.jpg" },
-        { images: "../assets/image/bannerTwo.jpg" },
-        { images: "../assets/image/bannerThree.jpg" },
-        { images: "../assets/image/bannerFour.jpg" },
+        { images: "../src/assets/image/bannerOne.jpg" },
+        { images: "../src/assets/image/bannerTwo.jpg" },
+        { images: "../src/assets/image/bannerThree.jpg" },
+        { images: "../src/assets/image/bannerFour.jpg" },
       ],
     };
   },

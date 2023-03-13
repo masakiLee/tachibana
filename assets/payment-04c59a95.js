6c059f750b12076f5333004427093d94bf492fdc
masakiLee <everything2427@hotmail.com>
2023-03-13 14:26:32 +0800

    deploy

---
+++ assets/payment-04c59a95.js
@@ -0,0 +1,1 @@
+import{by as e}from"./index-f20713f6.js";const a=e("payment",{state:()=>({success:!1}),actions:{setPaymentSuccess(s){this.success=s}}});export{a as p};
--- assets/payment-ceb8e49d.js
+++
@@ -1,1 +0,0 @@
-import{by as e}from"./index-2b5490d3.js";const a=e("payment",{state:()=>({success:!1}),actions:{setPaymentSuccess(s){this.success=s}}});export{a as p};

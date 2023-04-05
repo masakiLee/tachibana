66409af80c7e4733959fa9e6f420f3445b0e5b3b
masakiLee <everything2427@hotmail.com>
2023-04-05 17:46:35 +0800

    4/5

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
-/* global process */
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? /tachibana/ : '/',
+  base: '/tachibana/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
